f953de4d4460a5197e50a20d222a388e42a40ae0
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 20:44:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
   
  calls.any? {|call| call.length > 4}
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
