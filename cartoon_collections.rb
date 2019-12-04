2a7ce8e70880104f29443072a75cd035a9456732
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 20:44:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ def summon_captain_planet (planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
- 
+  
+ calls.any? {|call| call.length > 4}
 
 def find_the_cheese# code an argument here
   # the array below is here to help
